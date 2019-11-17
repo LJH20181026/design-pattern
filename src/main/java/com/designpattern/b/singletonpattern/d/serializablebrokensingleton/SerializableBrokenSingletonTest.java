2431dcab602699a511aa04c00e3efa1aa2a9ec86
LJH <1078103731@qq.com>
2019-11-17 14:18:26 +0800

    2019年11月17日14:18:00;

--- src/main/java/com/designpattern/b/singletonpattern/d/serializablebrokensingleton/SerializableBrokenSingletonTest.java
+++ src/main/java/com/designpattern/b/singletonpattern/d/serializablebrokensingleton/SerializableBrokenSingletonTest.java
@@ -1,10 +1,33 @@
 package com.designpattern.b.singletonpattern.d.serializablebrokensingleton;
 
+import java.io.*;
+
 public class SerializableBrokenSingletonTest {
 
     public static void main(String[] args) {
+        SerializableSingleton s1;
+        SerializableSingleton s2 = SerializableSingleton.getInstance();
 
+        try {
+            FileOutputStream fos = new FileOutputStream("SerializableSingleton.obj");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(s2);
+            oos.flush();
+            oos.close();
 
+            FileInputStream fis = new FileInputStream("SerializableSingleton.obj");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            s1 = (SerializableSingleton) ois.readObject();
+            ois.close();
+            fis.close();
+
+            System.out.println(s1);
+            System.out.println(s2);
+            System.out.println(s1 == s2);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/com/designpattern/b/singletonpattern/d/serializablebrokensingleton/SerializableSingleton.java
+++ src/main/java/com/designpattern/b/singletonpattern/d/serializablebrokensingleton/SerializableSingleton.java
@@ -2,13 +2,21 @@ package com.designpattern.b.singletonpattern.d.serializablebrokensingleton;
 
 import java.io.Serializable;
 
-public class SerializableSingleton {
+public class SerializableSingleton implements Serializable {
 
     private SerializableSingleton() {}
 
     private static final SerializableSingleton INSTANCE = new SerializableSingleton();
 
     public static SerializableSingleton getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     *
+     * @return
+     */
+    private Object readResolve() {
         return INSTANCE;
     }
 
